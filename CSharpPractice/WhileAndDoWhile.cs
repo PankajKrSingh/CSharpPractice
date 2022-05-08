be5e8b726e7c5aaa8e0a7b8b0423982576a5f9d2
Pankaj Kumar Singh <iampankajkrsingh@outlook.com>
2022-05-08 17:08:53 +0530

    add: while and do while loop

---
+++ CSharpPractice/WhileAndDoWhile.cs
@@ -0,0 +1,39 @@
+﻿namespace CSharpPractice;
+
+class WhileAndDoWhile
+{
+    static void Main(string[] args)
+    {
+        int tableOf = 2;
+
+        Console.WriteLine("USING WHILE LOOP");
+        Console.WriteLine("Printing table of 2 to 5");
+
+        while(tableOf <= 5)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Table Of : {tableOf}");
+            for(int i = 1; i<= 10; i++)
+            {
+                Console.WriteLine($"{tableOf} * {i} = {tableOf * i}");
+            }
+            tableOf++;
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("USING DO WHILE LOOP");
+        Console.WriteLine("Printing table as long as you want");
+
+        do
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Table Of : {tableOf}");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{tableOf} * {i} = {tableOf * i}");
+            }
+            tableOf++;
+            Console.WriteLine("Press enter to continue...");
+        } while (Console.ReadKey().Key == ConsoleKey.Enter);
+    }
+}
