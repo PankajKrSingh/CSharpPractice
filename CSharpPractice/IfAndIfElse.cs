a7fec160df52587ae8e0b7e4ba417764d9bcc64b
Pankaj Kumar Singh <iampankajkrsingh@outlook.com>
2022-05-11 17:24:26 +0530

    add: if and if else example

---
+++ CSharpPractice/IfAndIfElse.cs
@@ -0,0 +1,27 @@
+﻿namespace CSharpPractice
+{
+    class IfAndIfElse
+    {
+        public static void Main(string[] args)
+        {
+            // if and else if statement
+
+            int a = 2;
+            int b = 3;
+            int c = 4;
+
+            if(a > b)
+            {
+                Console.WriteLine("a is greater than b");
+            }
+            else if(b > c)
+            {
+                Console.WriteLine("b is greater than c");
+            }
+            else
+            {
+                Console.WriteLine("c is greater than both a and b");
+            }
+        }
+    }
+}
